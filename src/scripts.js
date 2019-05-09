d8d8300ac5bbe759c12f3d743ae378fc30ead5f8
Alek <alekaker@outlook.com>
2019-05-09 13:57:40 -0600

    data append to dom
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,18 @@
 $(document).ready(() => {
-  const $userPhoto = $('.user-photo');
-  const $allContent = $('.all-content');
-  const $dataCard = $('.user-data');
-  const $box1 = $('.average-steps');
-  const $box2 = $('.find-most-common-state');
-  const $box3 = $('.average-daily-ounces');
-  const $box4 = $('.specific-daily-ounces');
-  const $box5 = $('weekly-ounces');
-  // const $usersTestData = require('../data/users-test-data');
 
-
-
-
-  $userPhoto.click(function() {
+  $('.user-photo').click(function() {
     const $userRepository = new UserRepository(generateRandomUser());
     console.log($userRepository);
-    // $('.all-content').toggleClass('hidden');
-    $('.user-name').append('Name: ' + userRepository.currentUser.name);
-    $('.user-address').append('Address: ' + user.userData.address);
-    $('.user-email').append('Email: ' + user.userData.email);
+    $('.all-content').fadeToggle();
+    $('.user-name').append('Name: ' + $userRepository.currentUser.name);
+    $('.user-address').append('Address: ' + $userRepository.currentUser.address);
+    $('.user-email').append('Email: ' + $userRepository.currentUser.email);
+    $('.user-photo');
+    $('.all-content');
+    $('.user-data');
+    $('.average-steps');
+    $('.find-most-common-state');
+    $('.average-daily-ounces');
   });
   function generateRandomUser() {
     return Math.floor((Math.random() * 50) + 1);
