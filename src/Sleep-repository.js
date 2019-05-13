7d790ce0a78a9255ce6c8fe5d5fe58687f088057
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-13 09:00:52 -0600

    Add returnSleepQualityGreaterThan3fForWeek method
    Co-authored-by: Alek Aker <alekaker@outlook.com>

--- src/Sleep-repository.js
+++ src/Sleep-repository.js
@@ -1,6 +1,6 @@
 if (typeof module !== 'undefined') {
   Sleep = require('../src/Sleep');
-  sleepPathway = require('../data/sleep-test-data');
+  sleepPathway = require('../data/sleepRepo-test-data');
 } else {
   sleepPathway = sleepData;
 }
@@ -8,8 +8,41 @@ if (typeof module !== 'undefined') {
 class SleepRepository {
   constructor(userId) {
     this.filepathway = sleepPathway;
-    this.userSleepData = new Sleep();
+    this.userSleepData = new Sleep(this.returnSleepData(userId));
   }
+
+  returnSleepData(userId) {
+    return this.filepathway.find(sleepUser => sleepUser.userID == userId);
+  }
+
+  returnAveSleepQualityForAllUsers() {
+    return Number(this.filepathway.reduce((aveUserSleepQual, user) => {
+      return aveUserSleepQual += user.sleepData.reduce((totalQual, dailyQual) => {
+        return totalQual += dailyQual.sleepQuality / user.sleepData.length;
+      }, 0) / this.filepathway.length;
+    }, 0).toFixed(1));
+  }
+
+  returnSleepQualityGreaterThan3fForWeek() {
+    let a = sleepData.map(user => user.sleepData.findIndex(day => day.date == searchDate));
+    console.log('a', a);
+    let b = sleepData.map(user => user.sleepData.slice(a[0], a[0] + 7));
+    console.log('b', b);
+    let c = b.map(user => user.map(day => day.sleepQuality));
+    console.log('c', c);
+    let d = c.map(user => user.reduce((acc, curr) => acc += curr), 0);
+    console.log('d', d);
+    let e = d.map(user => user / 7);
+    console.log('e', e)
+    let f = e.reduce((acc, curr, index) => {
+      if (curr > 3) {
+        acc.push(index + 1);
+      }
+      return acc;
+    }, []);
+    console.log('f', f)
+  }
+
 }
 
 if (typeof module !== 'undefined') {
--- test/Sleep-repository-test.js
+++ test/Sleep-repository-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 const SleepRepository = require('../src/Sleep-repository');
-const Sleep = require('../src/Sleep');
+const sleepTestData = require('../data/sleepRepo-test-data');
 
 describe('SleepRepository', function() {
 
   let sleepRepository;
   beforeEach(function() {
-    sleepRepository = new SleepRepository(1);
-  })
+    sleepRepository = new SleepRepository(sleepTestData);
+  });
 
   it('should be a function', function() {
     expect(SleepRepository).to.be.a('function');
@@ -16,6 +16,14 @@ describe('SleepRepository', function() {
 
   it('should be an instance of SleepRepository', function() {
     expect(sleepRepository).to.be.an.instanceof(SleepRepository);
+  });
+
+  it('should have a method that returns the average sleep quality for all users', function() {
+    expect(sleepRepository.returnAveSleepQualityForAllUsers()).to.equal(3.1);
+  });
+
+  it('should have a method that returns all users whos average sleep quality is greater than 3 for a given 7 day week', function() {
+    expect(sleepRepository.returnSleepQualityGreaterThan3fForWeek()).to.eql();
   });
 
 });
