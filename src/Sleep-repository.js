df116592a3c9441c15a0d46fd581a184bd156b33
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-13 17:04:49 -0600

    Finish all sleep/sleeprepo methods
    Co-authored-by: Alek Aker <alekaker@outlook.com>

--- src/Sleep-repository.js
+++ src/Sleep-repository.js
@@ -24,8 +24,8 @@ class SleepRepository {
   }
 
   returnSleepQualityGreaterThan3fForWeek(searchDate) {
-    let dateIndex = sleepData.map(user => user.sleepData.findIndex(day => day.date == searchDate));
-    let weeklyData = sleepData.map(user => user.sleepData.slice(dateIndex[0], dateIndex[0] + 7));
+    let dateIndex = this.filepathway.map(user => user.sleepData.findIndex(day => day.date == searchDate));
+    let weeklyData = this.filepathway.map(user => user.sleepData.slice(dateIndex[0], dateIndex[0] + 7));
     let weeklySleepQual = weeklyData.map(user => user.map(day => day.sleepQuality));
     let totalSleepQual = weeklySleepQual.map(user => user.reduce((acc, curr) => acc += curr), 0);
     let aveSleepQual = totalSleepQual.map(user => user / 7);
@@ -35,6 +35,33 @@ class SleepRepository {
       }
       return acc;
     }, []);
+  }
+
+  returnHeaviestSleeperByDate(searchDate) {
+    let ids = [], hoursSlept = [], finalUsers = [];
+    this.filepathway.forEach(user => {
+      ids.push(user.userID);
+      user.sleepData.forEach(day => {
+        if (day.date === searchDate) {
+          hoursSlept.push(day.hoursSlept);
+        }
+      });
+    });
+    let maxHours = Math.max(...hoursSlept);
+    hoursSlept.forEach((el, i) => {
+      if (el === maxHours) {
+        finalUsers.push([ids[i], el]);
+      }
+    });
+    return finalUsers;
+  }
+
+  returnAveHoursSleptForAllUsers() {
+    return Number(this.filepathway.reduce((aveUserHoursSlept, user) => {
+      return aveUserHoursSlept += user.sleepData.reduce((totalHoursSlept, dailyHoursSlept) => {
+        return totalHoursSlept += dailyHoursSlept.hoursSlept / user.sleepData.length;
+      }, 0) / this.filepathway.length;
+    }, 0).toFixed(1));
   }
 
 }
--- test/Sleep-repository-test.js
+++ test/Sleep-repository-test.js
@@ -23,7 +23,15 @@ describe('SleepRepository', function() {
   });
 
   it('should have a method that returns all users whos average sleep quality is greater than 3 for a given 7 day week', function() {
-    expect(sleepRepository.returnSleepQualityGreaterThan3fForWeek()).to.eql();
+    expect(sleepRepository.returnSleepQualityGreaterThan3fForWeek('07/07/2019')).to.eql([ [ 1, 3.6 ], [ 2, 3.5 ] ]);
+  });
+
+  it('should have a method that returns, for a given day (identified by the date), find the users who slept the most number of hours (one or more if they tied)', function() {
+    expect(sleepRepository.returnHeaviestSleeperByDate('07/07/2019')).to.eql([ [ 2, 7.6 ] ]);
+  });
+
+  it('should have a method that returns the average hours slept for all users', function() {
+    expect(sleepRepository.returnAveHoursSleptForAllUsers()).to.eql(7.6);
   });
 
 });
